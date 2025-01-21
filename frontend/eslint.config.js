5d02dadff7f6c37982ed77178668a4f7a379207f
Aymane El Maini <elmainiaymane03@gmail.com>
2025-01-21 23:02:12 +0100

    update dockerfile again

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,47 +4,47 @@ const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
 
 module.exports = tseslint.config(
-  {
-    files: ["**/*.ts"],
-    extends: [
-      eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.stylistic,
-      ...angular.configs.tsRecommended,
-    ],
-    processor: angular.processInlineTemplates,
-    rules: {
-      quotes: ["error", "single"],
-      '@angular-eslint/component-class-suffix': 'error',
-      '@angular-eslint/directive-class-suffix': 'error',
-      '@angular-eslint/no-input-rename': 'error',
-      '@angular-eslint/no-output-rename': 'error',
-      '@angular-eslint/use-pipe-transform-interface': 'error',
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
+    {
+        files: ["**/*.ts"],
+        extends: [
+            eslint.configs.recommended,
+            ...tseslint.configs.recommended,
+            ...tseslint.configs.stylistic,
+            ...angular.configs.tsRecommended,
+        ],
+        processor: angular.processInlineTemplates,
+        rules: {
+            quotes: ["error", "single"],
+            '@angular-eslint/component-class-suffix': 'error',
+            '@angular-eslint/directive-class-suffix': 'error',
+            '@angular-eslint/no-input-rename': 'error',
+            '@angular-eslint/no-output-rename': 'error',
+            '@angular-eslint/use-pipe-transform-interface': 'error',
+            "@angular-eslint/directive-selector": [
+                "error",
+                {
+                    type: "attribute",
+                    prefix: "app",
+                    style: "camelCase",
+                },
+            ],
+            "@angular-eslint/component-selector": [
+                "error",
+                {
+                    type: "element",
+                    prefix: "app",
+                    style: "kebab-case",
+                },
+            ],
         },
-      ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
-        },
-      ],
     },
-  },
-  {
-    files: ["**/*.html"],
-    extends: [
-      ...angular.configs.templateRecommended,
-      ...angular.configs.templateAccessibility,
-    ],
-    rules: {},
-  }
+    {
+        files: ["**/*.html"],
+        extends: [
+            ...angular.configs.templateRecommended,
+            ...angular.configs.templateAccessibility,
+        ],
+        rules: {},
+    }
 );
 
