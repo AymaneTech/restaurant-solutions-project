088a58055995ba21f2384cf02f1ed4e72c2e3ba7
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-01-22 15:43:46 +0100

    Feat : make the component standalone
    let the menu-page componenet standalone and import the menu section component

--- frontend/src/app/features/customer/menu/pages/menu-page/menu-page.component.ts
+++ frontend/src/app/features/customer/menu/pages/menu-page/menu-page.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MenuSectionComponent} from '../../components/menu-section/menu-section.component';
 
 @Component({
   selector: 'app-menu-page',
-  imports: [],
+  imports: [
+    MenuSectionComponent
+  ],
   templateUrl: './menu-page.component.html',
+  standalone: true,
   styleUrl: './menu-page.component.css'
 })
 export class MenuPageComponent {
