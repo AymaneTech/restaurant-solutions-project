35ab1d009d22ffb55e00bbfeaa41009fe94856d8
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-01-22 11:56:14 +0100

    Feat : add new route
    set new route for the menu.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import {Routes} from '@angular/router';
 import {AdminLayoutComponent} from './core/layouts/admin-layout.component';
 import {CustomerLayoutComponent} from './core/layouts/customer-layout.component';
+import {MenuPageComponent} from './features/customer/menu/pages/menu-page/menu-page.component';
 
-export const routes: Routes = [
+export const routes: Routes =[
+  {
+    path: 'menu',
+    component: MenuPageComponent,
+  },
   {
     path: 'dashboard',
     component: AdminLayoutComponent,
