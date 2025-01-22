da9be5c0e2e422a6b82def6853e25a4f8c6471d5
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-01-22 09:52:07 +0100

    Feat : add the pages for menu
    set the routed component for the menu

---
+++ frontend/src/app/features/menu/pages/menu-page/menu-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-page',
+  imports: [],
+  templateUrl: './menu-page.component.html',
+  standalone: true,
+  styleUrl: './menu-page.component.css'
+})
+export class MenuPageComponent {
+
+}
