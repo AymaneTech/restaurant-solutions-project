c24501786e62f26ddd8cb6d8e53ad14af7e0ba1c
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-01-23 13:10:34 +0100

    Feat : add new component for order
    set new component for the oder item

---
+++ frontend/src/app/features/customer/order/components/order-item/order-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-item',
+  imports: [],
+  templateUrl: './order-item.component.html',
+  standalone: true,
+  styleUrl: './order-item.component.css'
+})
+export class OrderItemComponent {
+
+}
