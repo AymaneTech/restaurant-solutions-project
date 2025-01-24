9defa8636db6fbc24d01f9960fe485925938767e
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-01-24 10:58:57 +0100

    Feat : set the order panel

--- frontend/src/app/core/components/customer/customer-header/customer-header.component.ts
+++ frontend/src/app/core/components/customer/customer-header/customer-header.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import {OrderPageComponent} from '../../../../features/customer/order/pages/order-page/order-page.component';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-customer-header',
-  imports: [],
+  imports: [
+    OrderPageComponent,
+    NgIf
+  ],
   standalone: true,
   templateUrl: './customer-header.component.html',
 })
-export class CustomerHeaderComponent {}
+export class CustomerHeaderComponent {
+  showOrderPanel: boolean = false;
+
+  toggleOrderPanel(): void {
+    this.showOrderPanel = !this.showOrderPanel;
+  }
+}
