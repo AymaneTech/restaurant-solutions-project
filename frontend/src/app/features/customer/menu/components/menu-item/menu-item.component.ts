f15b89083b66def84487f25836f5e64b36793c7d
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-01-24 10:59:34 +0100

    set the menu item to can be ordered

--- frontend/src/app/features/customer/menu/components/menu-item/menu-item.component.ts
+++ frontend/src/app/features/customer/menu/components/menu-item/menu-item.component.ts
@@ -2,22 +2,33 @@ import { CommonModule } from '@angular/common'; // Importer CommonModule
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../../menu.model';
 import { ItemDetailsComponent } from '../item-details/item-details.component';
+import {OrderService} from '../../../order/order.service';
 
 @Component({
   selector: 'app-menu-item',
   imports: [CommonModule, ItemDetailsComponent],
   // import { Recipe } from './../../menu.model';
-templateUrl: './menu-item.component.html',
+  templateUrl: './menu-item.component.html',
 
+  standalone: true
 })
 export class MenuItemComponent {
   @Input() recipe!: Recipe;
   showPopup = false;
-  openPopup(): void {    
+
+  constructor(private orderService: OrderService ) {}
+
+  openPopup(): void {
     this.showPopup = true;
   }
 
   closePopup() {
     this.showPopup = false;
   }
+
+  addToOrder() {
+    this.orderService.addToOrder(this.recipe);
+    // alert(`${this.recipe.name} added to your order!`);
+  }
+
 }
