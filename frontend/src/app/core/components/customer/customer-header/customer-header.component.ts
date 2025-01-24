9ef4b3dd386ea8f408c9ab800b7c3d0a14baa342
Aymane El Maini <elmainiaymane03@gmail.com>
2025-01-24 11:13:34 +0100

    import other modules

--- frontend/src/app/core/components/customer/customer-header/customer-header.component.ts
+++ frontend/src/app/core/components/customer/customer-header/customer-header.component.ts
@@ -1,9 +1,11 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {OrderPageComponent} from '../../../../features/customer/order/pages/order-page/order-page.component';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-customer-header',
-  imports: [CommonModule],
+  imports: [CommonModule, OrderPageComponent, RouterLink],
   standalone: true,
   templateUrl: './customer-header.component.html',
 })
