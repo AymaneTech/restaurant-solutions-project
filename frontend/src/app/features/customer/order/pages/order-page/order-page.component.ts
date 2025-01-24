6e3f7130be94365312871b2bcc3da32971201c80
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-01-24 11:00:46 +0100

    Feat : set the order page to have the ordered item and the total

--- frontend/src/app/features/customer/order/pages/order-page/order-page.component.ts
+++ frontend/src/app/features/customer/order/pages/order-page/order-page.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {OrderItemComponent} from '../../components/order-item/order-item.component';
+import {Recipe} from '../../../menu/menu.model';
+import {OrderService} from '../../order.service';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-order-page',
   imports: [
-    OrderItemComponent
+    OrderItemComponent,
+    NgForOf
   ],
   templateUrl: './order-page.component.html',
   standalone: true,
   styleUrl: './order-page.component.css'
 })
 export class OrderPageComponent {
+  orderItems: Recipe[] = [];
+  totalPrice = 0;
+
+  constructor(private orderService: OrderService) {
+    this.orderService.orderItems$.subscribe((items) => {
+      this.orderItems = items;
+      this.totalPrice = this.orderService.getOrderTotal();
+    });
+  }
+
+  removeItem(index: number) {
+    this.orderService.removeFromOrder(index);
+  }
 
 }
