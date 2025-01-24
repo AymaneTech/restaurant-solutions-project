f3c6285e48bae71eec68853467092e920c9a4f58
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-01-24 16:15:16 +0100

    Feat : set new method to add or reduce item quantity

--- frontend/src/app/features/customer/order/components/order-item/order-item.component.ts
+++ frontend/src/app/features/customer/order/components/order-item/order-item.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {OrderItem} from '../../../menu/menu.model';
 import {Store} from '@ngrx/store';
-import {removeOrderItem} from '../../../store/customer.actions';
+import {addOrderItemQuantity, reduceOrderItemQuantity, removeOrderItem} from '../../../store/customer.actions';
 
 @Component({
   selector: 'app-order-item',
@@ -19,5 +19,13 @@ export class OrderItemComponent {
 
   removeItem(): void {
     this.store.dispatch(removeOrderItem({item: this.item}));
+  }
+
+  reduceQuantity() {
+    this.store.dispatch(reduceOrderItemQuantity({ itemId: this.item.recipe.id }));
+  }
+
+  addQuantity() {
+    this.store.dispatch(addOrderItemQuantity({ itemId: this.item.recipe.id }));
   }
 }
